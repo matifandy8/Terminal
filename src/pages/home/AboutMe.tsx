d9aae96a7e42c7d8fcd7c2d8653aa837a46270ce
matias <matifandy@gmail.com>
2021-12-13 14:56:44 -0300

    add terminal form and aboutme

---
+++ src/pages/home/AboutMe.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const AboutMe = () => {
+    return (
+        <div>
+            <h1>About Me</h1>
+            <p>Hello i am Matias Fandiño</p>
+            <p>I am a Frontend Developer</p>
+            <p>I am a Self-Taught i have experience developing web applications using React, Redux, Javascript, MongoDB and Node.js.</p>
+            <p> My passion is to make applications with clean and beautiful UI design for a great user experience.</p>
+        </div>
+    )
+}
+
+export default AboutMe
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,11 +1,53 @@
-import React from 'react'
+import { useState } from "react";
+import AboutMe from "./AboutMe";
 
 const Home = () => {
-    return (
-        <div>
-            <h1>Home</h1>
-        </div>
-    )
-}
+    const [value, setValue] = useState("");
+    const [tag, setTag] = useState("");
 
-export default Home
+    const handleChange = (e:any) => {
+      setValue(e.target.value);
+    };
+
+    const onSubmitForm = (e:any) => {
+        e.preventDefault();
+        console.log(value);
+        if(value === ""){
+            alert("Please enter a value");
+        }
+        else{
+            setTag(value);
+            setValue("");
+        }
+      };
+  
+    const handleKeypress = (e:any) => {
+      if (e.keyCode === 13) {       
+        onSubmitForm(e);
+      }
+    };
+
+
+  return (
+    <div className="home">
+      <div className="terminal">
+        <div className="terminal__form">
+        <span>$</span>
+        <form onSubmit={onSubmitForm} >
+          <input
+            value={value}
+            onChange={handleChange}
+            onKeyPress={handleKeypress}
+            type="text"
+          />
+        </form>
+        </div>
+        <div className="terminal__output">
+          {tag === "about" ? <AboutMe /> : ""}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
