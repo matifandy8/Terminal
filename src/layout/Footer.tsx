f2ff7e0f046b3be88f83228193f52dfe0bba33f4
matias <matifandy@gmail.com>
2021-12-14 19:08:32 -0300

    add modal footer

--- src/layout/Footer.tsx
+++ src/layout/Footer.tsx
@@ -1,9 +1,31 @@
+import { useState } from "react";
+import Modal from "react-modal";
+
+Modal.setAppElement("#root");
 
 const Footer = () => {
-    return (
-        <div className='footer'>
-        </div>
-    )
-}
+  const [isOpen, setIsOpen] = useState(false);
 
-export default Footer
+  function toggleModal() {
+    setIsOpen(!isOpen);
+  }
+
+  return (
+    <footer className="footer">        
+        <button className="btn" onClick={toggleModal}>Help</button>
+
+        <Modal
+          isOpen={isOpen}
+          onRequestClose={toggleModal}
+          contentLabel="My dialog"
+          portalClassName="modal"
+          overlayClassName="overlay"
+          >
+          <div>My modal dialog.</div>
+          <button onClick={toggleModal}>Close modal</button>
+        </Modal>
+    </footer>
+  );
+};
+
+export default Footer;
