7551b4cb2ac7b9b848f091661d3d3190af2fa4aa
matias <matifandy@gmail.com>
2022-03-22 17:03:18 -0300

    feedback applied

--- src/layout/Footer.tsx
+++ src/layout/Footer.tsx
@@ -1,63 +1,12 @@
-import { useState } from "react";
 import Modal from "react-modal";
 
-Modal.setAppElement("#root");
 
 const Footer = () => {
-  const [isOpen, setIsOpen] = useState(false);
 
-  function toggleModal() {
-    setIsOpen(!isOpen);
-  }
 
   return (
     <footer className="footer">        
-        <button className="btn" onClick={toggleModal}>Help</button>
-
-        <Modal
-          isOpen={isOpen}
-          onRequestClose={toggleModal}
-          contentLabel="My dialog"
-          portalClassName="modal"
-          overlayClassName="overlay"
-          style={{
-            overlay: {
-              position: 'fixed',
-              top: 100,
-              left: 150,
-              right: 0,
-              bottom: 0,
-              width: 400,
-              height: 400,
-              backgroundColor: 'rgba(0, 0, 0, 0.75)'
-            },
-            content: {
-              position: 'absolute',
-              top: '40px',
-              left: '40px',
-              right: '40px',
-              bottom: '40px',
-              border: '1px solid #ccc',
-              background: '#000',
-              overflow: 'auto',
-              WebkitOverflowScrolling: 'touch',
-              borderRadius: '4px',
-              outline: 'none',
-              padding: '20px',
-              color: '#00ff55'
-            }
-          }}
-          >
-            <div className="help">
-              <h2>Help</h2>
-              <p>Run the commands to see my information:</p>
-              <p>about me (ab)</p>
-              <p>projects (pjs)</p>
-              <p>contact (ct)</p>
-              <button onClick={toggleModal}>Close modal</button>
-            </div>
-          
-        </Modal>
+        
     </footer>
   );
 };
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -32,22 +32,32 @@ const Home = () => {
     <div className="home">
       <div className="terminal">
         <div className="terminal__form">
-          <form onSubmit={onSubmitForm}>    
-            <span>$</span>
+          <form onSubmit={onSubmitForm}>
+          <span >C:/User/&gt;</span>
             <input
               value={value}
               onChange={handleChange}
               onKeyPress={handleKeypress}
               type="text"
+              autoFocus
             />
           </form>
         </div>
         <div className="terminal__output">
           <div className="title">
-            <h1 >Hi, This is my portfolio interactive terminal with commands in help menu to navigate through the website and know more about me.</h1>
-          <p>Used technologies React, Typescript, Sass</p>
+            <h1>
+              Hi, This is my portfolio interactive terminal with commands in
+              help menu to navigate through the website and know more about me.
+            </h1>
+            <p>Used technologies React, Typescript, Sass</p>
+            <p>
+              Run the commands to see my information:
+              <p>about me (ab)</p>
+              <p>projects (pjs)</p>
+              <p>contact (ct)</p>
+            </p>
           </div>
-          
+
           {tag === "ab" ? <AboutMe /> : ""}
           {tag === "pjs" ? <Projects /> : ""}
           {tag === "ct" ? <Contact /> : ""}
